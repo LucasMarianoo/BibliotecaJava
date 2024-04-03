c27beb93dc82b2bee725c5a06e4dae90a5c2e4fb
Lucas <lucas.mariano1991@outlook.com>
2024-04-02 21:36:31 -0300

    novo commit

---
+++ src/biblioteca/model/bean/Cliente.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package biblioteca.model.bean;
+
+/**
+ *
+ * @author skynn
+ */
+public class Cliente {
+    
+}
